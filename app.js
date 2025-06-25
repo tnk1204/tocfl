313d8c5a0fa474767d5ab9b59fa3189c7dee5681
tnk1204 <imteaak3@gmail.com>
2025-06-25 15:06:39 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+// ... (all JavaScript logic from index.html, except vocabData array)
+// Ensure all references to vocabData work as global from data.js
+}); 
